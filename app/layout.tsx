0f26500a17ea646335570d8cf1767b6a896f794e
josh <josh@afterima.ge>
2025-03-19 17:29:10 +0000

    mystery

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,18 +17,37 @@ export const metadata = {
       { url: '/bunny-16.png', sizes: '16x16', type: 'image/png' },
       { url: '/bunny-32.png', sizes: '32x32', type: 'image/png' },
       { url: '/bunny-96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: [
       { url: '/bunny-57.png', sizes: '57x57', type: 'image/png' },
       { url: '/bunny-60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/bunny-70.png', sizes: '70x70', type: 'image/png' },
       { url: '/bunny-72.png', sizes: '72x72', type: 'image/png' },
       { url: '/bunny-76.png', sizes: '76x76', type: 'image/png' },
+      { url: '/bunny-100.png', sizes: '100x100', type: 'image/png' },
     ],
     other: [
       { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+      { rel: 'icon', type: 'image/x-icon', url: '/favicon.ico' },
     ]
   },
   manifest: '/site.webmanifest',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "bunnyrabbit",
+  }
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: '#ffffff',
 }
 
 export default function RootLayout({
